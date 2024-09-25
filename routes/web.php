d18f0bdb388801490172f6bf1286a8f2581fe260
codeMountainEs <codemountain.es@icloud.com>
2024-09-26 00:33:55 +0200

    12

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+//Route::view('/', 'welcome');
+Route::get('/', function () {
+    return redirect('/admin');
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
