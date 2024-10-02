1f1bbac5d63f1fa5b027309e94eb949607bd7875
codeMountainEs <codemountain.es@icloud.com>
2024-10-02 22:48:20 +0200

    mensaje

--- resources/views/livewire/accesos/create.blade.php
+++ resources/views/livewire/accesos/create.blade.php
@@ -258,35 +258,7 @@ new class extends Component {
                 </div>
                 @endif
 
-                <div class="sm:flex mt-3 sm:justify-center sm:items-center text-center sm:text-start">
-                    <div x-data="{ showMessage: true }" x-init="setTimeout(() => showMessage = false, 3000)">
-                        @if (session()->has('message'))
-                            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert"  
-                                 x-show="showMessage">
-                                <strong class="font-bold">Correcto!</strong>
-                                <span class="block sm:inline">{{ session('message') }}</span>
-                                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                                        <svg class="fill-current  
-                     h-6 w-6 text-green-500"  
-                                             fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M5 13l14 14-1.42 1.42L4 4h16v12H5z"/></svg>
-                                    </span>
-                            </div>
-                        @elseif (session()->has('error'))
 
-                            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert"  
-                                 x-show="showMessage">
-                                <strong class="font-bold">ERROR!</strong>
-                                <span class="block sm:inline">{{ session('error') }}</span>
-                                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                                        <svg class="fill-current  
-                     h-6 w-6 text-green-500"  
-                                             fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M5 13l14 14-1.42 1.42L4 4h16v12H5z"/></svg>
-                                    </span>
-                            </div>
-                        @endif
-
-                    </div>
-                </div>
 
 
 
